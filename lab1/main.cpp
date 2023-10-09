e9ee3f9dcc8527e7e7acc2d473b2b842e1e20add
Азя <azyadr@yandex.ru>
2023-10-10 01:26:03 +0300

    v2.0

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 // функция для методов
 double function(double var) {
-    return pow(tan(var), 3) - var + 1;
+    return pow(var, 5) + pow(var, 2) - 5;
 }
 
 // Класс, реализующий метод половинного деления
@@ -22,8 +23,6 @@ public:
 
         while((b - a) / 2 > e) {
             result = (a + b) / 2;
-
-            std::cout << "a = " << a << " b = " << b << " result = " << result << '\n';
 
             if ((function(a) * function(result)) > 0) a = result;
             else b = result;
@@ -47,8 +46,6 @@ public:
         while (std::abs(b - a) > e) {
             a = a - (b - a) * function(a) / (function(b) - function(a));
             b = b - (a - b) * function(b) / (function(a) - function(b));
-
-            std::cout << "a = " << a << " b = " << b << " a - b = " << a - b << '\n';
         }
 
         return a;
@@ -58,9 +55,17 @@ public:
 int main() {
     system("chcp 1251");
 
-    std::cout << "Метотд половинного деления:\n";
-    std::cout << MPD().run(-1.5, 0.5);
-    std::cout << "\nМетотд хорд:\n";
-    std::cout << MH().run(-1.5, 0.5);
+    std::cout.setf(std::ios::fixed);
+
+    for (int index = 2; index <= 15; index++) {
+        std::cout << std::setprecision(8) << std::setw(10) << MPD(pow(10, -1 * index)).run(-1000, 1000) << ' ';
+    }
+
+    std::cout << '\n';
+
+//    for (int index = 2; index <= 15; index++) {
+//        std::cout << std::setprecision(8) << std::setw(10) << MH(pow(10, -1 * index)).run(-1.5, 0.5) << ' ';
+//    }
+
     return 0;
 }
